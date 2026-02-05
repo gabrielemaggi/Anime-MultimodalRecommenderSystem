72167ed13245d9a042c2cdd4d280d0272afdd641
OmerSherman <omersherman22@gmail.com>
2026-02-05 16:03:52 +0100

    Imported User_last_watch_df from feature/userEmbedding

---
+++ User_last_watch_df.py
@@ -0,0 +1,44 @@
+import os.path
+import pandas as pd
+from UserDBConnector import UserDBConnector
+class User_test_df:
+    """
+        a class to get a data frame of the user watched
+        """
+    def __init__(self, last_k_animes_to_predict =  1):
+        self.userAnime_last_watched = self._create_user_last_watched_df(last_k_animes_to_predict)
+
+    filterd_df_last_watched_path = "Resources/user_watches_filtered.parquet"
+    userAnime_df_path = "Resources/UserAnimeList.parquet"
+
+    def _create_user_filtered_df(self):
+        if(not os.path.exists(User_test_df.userAnime_df_path)):
+            UserDBConnector()
+        userAnime_df = pd.read_parquet(User_test_df.userAnime_df_path)
+        cols = ['anime_id', 'my_score']
+        userAnime_df['id_score_pair'] = userAnime_df[cols].values.tolist()
+        grouped = userAnime_df.groupby('username')['id_score_pair'].agg(list).reset_index()
+        filtered_users = grouped[grouped['id_score_pair'].apply(len) > 1].copy()
+        filtered_users.to_parquet(User_test_df.filterd_df_last_watched_path)
+        self.filterd_df = filtered_users
+
+    def _create_user_last_watched_df(self, k):
+        if(not os.path.exists(User_test_df.filterd_df_last_watched_path)):
+            self._create_user_filtered_df()
+        else:
+            self.filterd_df = pd.read_parquet(User_test_df.filterd_df_last_watched_path)
+            self.filterd_df['id_score_pair'] = self.filterd_df['id_score_pair'].apply(
+                lambda row: [item.tolist() for item in row]
+            )
+        self.filterd_df = self.filterd_df[self.filterd_df['id_score_pair'].apply(len) > 3*k]
+        self.filterd_df['watched_anime'] = self.filterd_df['id_score_pair'].apply(lambda x: x[:-k])
+        self.filterd_df['lastwatched'] = self.filterd_df['id_score_pair'].apply(lambda x: x[-k:])
+        self.filterd_df['lastwatched'] = self.filterd_df['lastwatched'].apply(lambda x : [pair for pair in x if pair[1] > 6])
+        self.filterd_df = self.filterd_df[self.filterd_df['lastwatched'].apply(len) > 0]
+
+    def getDataFrame(self):
+        return self.filterd_df
+
+if __name__ =='__main__':
+    u = User_test_df(2)
+    u.getDataFrame()
